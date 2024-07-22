d3a58ccd0022e5fd8b70b3ed1635d755ea4dc454
verowangari <verowangari34@gmail.com>
2024-07-22 10:56:53 +0300

    display country population

--- src/components/AllCountries/AllCountries.jsx
+++ src/components/AllCountries/AllCountries.jsx
@@ -1,65 +1,110 @@
+import React, { useState, useEffect } from "react";
+import { apiURL } from "../util/api";
 
+import SearchInput from "../Search/SearchInput";
+import FilterCountry from "../FilterCountry/FilterCountry";
 
-import React, {useState,useEffect} from 'react';
-import { apiURL } from '../util/api';
+import { Link } from "react-router-dom";
 
 const AllCountries = () => {
-    const[countries,setCountries]=useState([])
-    const[isLoading,setIsLoading]=useState(true)
-    const[erros,setErroe]=useState('')
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
+  const getAllCountries = async () => {
+    try {
+      const res = await fetch(`${apiURL}/all`);
 
-    constgetAllCountries=async()=>{
-        try {
-            const res= await fetch('${apiURL}/all');
+      if (!res.ok) throw new Error("Something went wrong!");
 
+      const data = await res.json();
 
-            if(!res.ok) throw new Error( 'Something went wrong!');
+      console.log(data);
 
-            const data=await res.json();
-            console.log(data);
+      setCountries(data);
 
-             setCountries(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
 
-             setIsLoading(false);
-            
-        } catch (error) {
-            setIsLoading(false);
-            setError(error.message);
-        }
-    };
+  const getCountryByName = async (countryName) => {
+    try {
+      const res = await fetch(`${apiURL}/name/${countryName}`);
 
-    useEffect(()=>{
-        getAllCountries();
-    },[]);
+      if (!res.ok) throw new Error("Not found any country!");
+
+      const data = await res.json();
+      setCountries(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  const getCountryByRegion = async (regionName) => {
+    try {
+      const res = await fetch(`${apiURL}/region/${regionName}`);
+
+      if (!res.ok) throw new Error("Failed..........");
+
+      const data = await res.json();
+      setCountries(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
   return (
-    <div className='all__country__wrapper'>
-        <div className="country__top">
-
+    <div className="all__country__wrapper">
+      <div className="country__top">
+        <div className="search">
+          <SearchInput onSearch={getCountryByName} />
         </div>
+
+        <div className="filter">
+          <FilterCountry onSelect={getCountryByRegion} />
+        </div>
+      </div>
+
       <div className="country__bottom">
-{isLoading&& !error && <h4>Loading.......</h4>}
-{error && !isLoading && <h4>{error}</h4>}
+        {isLoading && !error && <h4>Loading........</h4>}
+        {error && !isLoading && <h4>{error}</h4>}
 
-{
-    countries?.map(country=>(
-        <div className="country__card">
-            <div className="country__img">
-<img src= {country.flags.png}alt=''   />
-            </div>
+        {countries?.map((country) => (
+          <Link to={`/country/${country.name.common}`}>
+            <div className="country__card">
+              <div className="country__img">
+                <img src={country.flags.png} alt="" />
+              </div>
 
-            <div className="country__data">
+              <div className="country__data">
                 <h3>{country.name.common}</h3>
-                <h6>Population:{country.population}</h6>
-                <h6>Region:{country.region}</h6>
-                <h6>Capital:{country.capital}</h6>
+                <h6>
+                  {" "}
+                  Population:{" "}
+                  {new Intl.NumberFormat().format(country.population)}
+                </h6>
+                <h6> Region: {country.region}</h6>
+                <h6>Capital: {country.capital}</h6>
+              </div>
             </div>
-        </div>
-    ))
-}
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllCountries
+export default AllCountries;
